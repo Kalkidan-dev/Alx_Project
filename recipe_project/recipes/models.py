3d21e01ae433aff7f370020a94c35a770732ac74
Kalkidan-dev <cakek433@gmail.com>
2025-08-16 14:35:10 +0300

    Created three models which is Category, Ingredient and Recipe

--- recipe_project/recipes/models.py
+++ recipe_project/recipes/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+    quantity = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name} ({self.quantity})"
+
+# Ensure validations for required fields such as Title, Ingredients, and Instructions.
+class Recipe(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipes')
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    ingredients = models.ManyToManyField(Ingredient)
+    instructions = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    preparation_time = models.PositiveIntegerField(help_text="Time in minutes")
+    cooking_time = models.PositiveIntegerField(help_text="Time in minutes")
+    servings = models.PositiveIntegerField(default=1)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
