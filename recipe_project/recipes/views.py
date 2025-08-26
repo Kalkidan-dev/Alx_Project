8cd2edd7488f279636084d2c6215a5bf731289b0
Kalkidan-dev <cakek433@gmail.com>
2025-08-26 16:46:32 +0300

    Implement and adjust recipe API views

--- recipe_project/recipes/views.py
+++ recipe_project/recipes/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from django.contrib.auth.models import User
+from .models import Category, Ingredient, Recipe
+from .serializers import (
+    CategorySerializer,
+    IngredientSerializer,
+    RecipeSerializer,
+    UserSerializer,
+)
 
-# Create your views here.
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class IngredientViewSet(viewsets.ModelViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    queryset = Recipe.objects.all().select_related("owner", "category").prefetch_related("recipeingredient_set__ingredient")
+    serializer_class = RecipeSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        # Ensure recipe.owner is set to current user
+        serializer.save(owner=self.request.user)
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
