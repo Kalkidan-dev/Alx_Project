92107bcb1e3d5b22907b957be2a6eaac96f1daa3
Kalkidan-dev <cakek433@gmail.com>
2025-08-30 16:18:04 +0300

    Replace the user with Custome user and give permission for authenticated users

--- recipe_project/recipes/views.py
+++ recipe_project/recipes/views.py
@@ -1,5 +1,4 @@
 from rest_framework import viewsets, permissions
-from django.contrib.auth.models import User
 from .models import Category, Ingredient, Recipe
 from .serializers import (
     CategorySerializer,
@@ -7,30 +6,33 @@ from .serializers import (
     RecipeSerializer,
     UserSerializer,
 )
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class IngredientViewSet(viewsets.ModelViewSet):
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all().select_related("owner", "category").prefetch_related("recipeingredient_set__ingredient")
     serializer_class = RecipeSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticated]
 
     def perform_create(self, serializer):
-        # Ensure recipe.owner is set to current user
         serializer.save(owner=self.request.user)
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
