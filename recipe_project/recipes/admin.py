9759d3a8cf7533b43d72a0bb927cecff748efeae
Kalkidan-dev <cakek433@gmail.com>
2025-08-30 16:14:02 +0300

    Register the recipe models in admin side

--- recipe_project/recipes/admin.py
+++ recipe_project/recipes/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Recipe, Category, Ingredient, RecipeIngredient
 
-# Register your models here.
+# ----------------- Recipe Management -----------------
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'owner', 'category', 'created_at')
+    search_fields = ('title', 'owner__username', 'category__name')
+    list_filter = ('category',)
+    ordering = ('-created_at',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+@admin.register(RecipeIngredient)
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'ingredient', 'quantity')
+    search_fields = ('recipe__title', 'ingredient__name')
+    ordering = ('recipe',)
